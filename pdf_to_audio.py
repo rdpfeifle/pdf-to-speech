dc8f4367c8716288b555a552dfee03e4058dcb90
rdpcodes <54380958+rdpfeifle@users.noreply.github.com>
2023-12-14 10:37:07 -0700

    Create function to convert text to speech

--- pdf_to_audio.py
+++ pdf_to_audio.py
@@ -1,4 +1,5 @@
 from PyPDF2 import PdfReader
+from gtts import gTTS
 
 def extract_text_from_pdf(file_to_read):
     """
@@ -23,9 +24,30 @@ def extract_text_from_pdf(file_to_read):
 
             # Extract text from the current page
             extracted_text = page.extract_text()
-            
+
             # If text was found, concatenate it to the combined text
             if extracted_text:
                 combined_text += extracted_text + "\n"
-
+    
     return combined_text
+
+
+def convert_text_to_speech(text, audio_file_name):
+    """
+    Converts the text into an audio file and saves it.
+
+    Arguments: 
+    text (str): The text to be converted to speech.
+    audio_file_name (str): The name of the output audio file, including the file extension (e.g., '.mp3').
+
+    Returns:
+    Saves the audio to a file.
+    """
+
+    language = "en"
+
+    # Create an audio with the specified language and speed settings
+    audio = gTTS(text=text, lang=language, slow=False)
+
+    audio.save(audio_file_name)
+
