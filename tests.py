915baada8995c19822561a3e5d5dcb6701048c8d
rdpcodes <54380958+rdpfeifle@users.noreply.github.com>
2023-12-15 11:46:29 -0700

    Create two unittest files

---
+++ tests.py
@@ -0,0 +1,27 @@
+import unittest
+import pdf_to_audio
+
+test_pdf_path = "./Developer_Jokes.pdf"
+
+# Extract text from the extract_text_from_pdf function
+extracted_text = pdf_to_audio.extract_text_from_pdf(test_pdf_path)
+
+class Pdf_to_Speech_TestCase(unittest.TestCase):
+    """Tests for PDF to Speech extraction."""
+
+    def test_extract_text_from_pdf(self):
+        """Text if text is being extracted from pdf."""
+
+        expected_text = "developers"
+
+        assert expected_text in extracted_text
+
+
+    def test_pdf_file_is_not_empty(self):
+        """Test if the pdf file is not empty and contains text."""
+
+        self.assertTrue(len(extracted_text) > 0)
+
+
+if __name__ == "__main__":
+    unittest.main()
